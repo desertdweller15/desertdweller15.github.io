0cad180648ebcc6d36419ce9740ed01920eea6b6
desertdweller15 <bcoutee11@gmail.com>
2016-10-17 14:36:53 -0700

    General Assembly JS Course Work

---
+++ JavaScript Course GA/Unit 2/Unit 2_Code Challenge 3_Brian.js
@@ -0,0 +1,120 @@
+
+NEW
+.
+Folder Path
+Shared with me
+Unit 2
+Folders and views
+My Drive
+Shared with me
+Recent
+Google Photos
+Starred
+Trash
+Get Drive for PC
+2 GB of 15 GB used
+Upgrade storage
+NAME
+Files
+.
+
+Javascript
+unit-2_fizzbuzz-pre-project-optional-practice_seah-liwei.js
+
+Javascript
+unit-2_lesson-1-code-challenge-comparison-and-logical-operators_seah-liwei.js
+
+Javascript
+unit-2_lesson-2-code-challenge-conditionals-game_seah-liwei.js
+
+Javascript
+unit-2_lesson-3-code-challenge-looping_seah-liwei.js
+
+Javascript
+unit-2_lesson-4-code-challenge-switch-and-ternary-statements_seah-liwei.js
+
+Javascript
+unit-2_unit-2-project_seah-liwei.js
+
+PDF
+unit-2-review-guide.pdf
+Javascript
+unit-2_lesson-3-code-challenge-looping_seah-liwei.js
+Details
+Activity
+unit-2_lesson-3-code-challenge-looping_seah-liwei.js
+Sharing Info
+S
+General Info
+Type
+Javascript
+Size
+891 bytes
+Storage used
+0 bytesOwned by someone else
+Location
+Unit 2
+Owner
+Seah Li-wei
+Modified
+Oct 7, 2016 by Seah Li-wei
+Opened
+4:35 PM by me
+Created
+Oct 7, 2016
+Description
+No description
+Download permissions
+Viewers can download
+
+
+var oldArray = [12, 45, 6, 23, 19, 20, 20, 15, 30, 42];
+
+// Write your code below this line
+var newArray = [];
+
+/*for(i = 0; i < oldArray.length; i++){
+if (i % 2 === 0){
+    newArray.push(oldArray[i]);
+  } else {
+      newArray.push(oldArray[i] + 5);
+  }
+}*/
+
+/*for(i = 0; i < oldArray.length; i++){
+if (i % 2 === 0){
+    newArray.push(oldArray[i]);
+  } else {
+      newArray.push(oldArray[i] * oldArray[i]);
+  }
+}*/
+
+/*for(i = 0; i < oldArray.length; i++){
+if (i % 2 === 0){
+    newArray.push(oldArray[i]);
+  } else {
+      newArray.push(oldArray[i] * 2);
+  }
+}*/
+
+//console.log(newArray);
+
+var x = 0;
+var greeting = "";
+
+/*while(x < 6){
+greeting += "hello ";
+  x++;
+}*/
+
+while(x < 10){
+if(x % 2 === 0){
+    greeting += "hello ";
+    x++;
+} else {
+  greeting += "goodbye ";
+  x++;
+}
+};
+
+console.log(greeting);
